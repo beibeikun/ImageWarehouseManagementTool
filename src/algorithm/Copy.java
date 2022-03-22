fe9be5c40c998c141ccf85745be09002ec25111a
beibeikun <cjzhelloworld@outlook.com>
2022-03-22 14:39:50 +0900

    test

---
+++ src/algorithm/Copy.java
@@ -0,0 +1,33 @@
+package algorithm;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Copy
+{
+    public void copy(String srcPathStr, String desPathStr)
+    {
+        //获取源文件的名称
+        String newFileName = srcPathStr.substring(srcPathStr.lastIndexOf("\\")+1); //目标文件地址
+        desPathStr = desPathStr + File.separator + newFileName; //源文件地址
+        try
+        {
+            FileInputStream fis = new FileInputStream(srcPathStr);//创建输入流对象
+            FileOutputStream fos = new FileOutputStream(desPathStr); //创建输出流对象
+            byte datas[] = new byte[1024*8];//创建搬运工具
+            int len = 0;//创建长度
+            while((len = fis.read(datas))!=-1)//循环读取数据
+            {
+                fos.write(datas,0,len);
+            }
+            fos.close();//释放资源
+            fis.close();//释放资源
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
