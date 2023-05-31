8f84524cbcc38b2adfd507bd3c4361a67f9e7097
beibeikun <cjzhelloworld@outlook.com>
2023-05-31 19:03:48 +0900

    new ui

--- src/UIPAGE/MainpageUI.java
+++ src/UIPAGE/MainpageUI.java
@@ -1,27 +1,37 @@
 package UIPAGE;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
-import com.formdev.flatlaf.FlatLaf;
-import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
 
 public class MainpageUI {
     private JPanel panel1;
-    private JComboBox comboBox1;
     private JTabbedPane tabbedPane1;
+    private JButton 检查正确性Button;
+    private JButton 开始重命名Button;
+    private JCheckBox 使用三位lot号CheckBox;
+    private JCheckBox 添加Lot前缀CheckBox;
+    private JCheckBox 完成后按文件夹分类CheckBox;
+    private JCheckBox 尝试从库中提取缺少的拍品CheckBox;
+    private JList list1;
+    private JList list2;
+    private JList list3;
+    private JButton 选择Button1;
+    private JButton 选择Button2;
+    private JButton 选择Button;
+    private JCheckBox 替换已存在的图片CheckBox;
+    private JButton 开始添加Button;
+    private JButton 开始移除Button;
+    private JTextField textField1;
+    private JButton 查询Button;
     private JCheckBox checkBox1;
-    private JCheckBox checkBox2;
-    private JCheckBox checkBox3;
-    private JButton button1;
-    private JButton button2;
 
     public static void main(String[] args) {
         FlatDarculaLaf.setup();
         JFrame frame = new JFrame("MainpageUI");
         frame.setContentPane(new MainpageUI().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400,500);
+        frame.setSize(500,350);
         frame.setVisible(true);
     }
 }
