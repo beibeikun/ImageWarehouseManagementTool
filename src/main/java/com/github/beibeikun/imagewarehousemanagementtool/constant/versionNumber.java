9c3c3db27da2527ccd01ab8c042c8f90d557a708
beibeikun <cjzhelloworld@outlook.com>
2024-10-28 16:09:08 +0900

    Refactor File Structure

--- src/main/java/com/github/beibeikun/imagewarehousemanagementtool/constant/versionNumber.java
+++ src/main/java/com/github/beibeikun/imagewarehousemanagementtool/constant/versionNumber.java
@@ -4,7 +4,7 @@ public class versionNumber
 {
     public static final String VERSION_NUMBER = "version Number";
     public static final String OFFICIAL_TITLE = "Build: V";
-    public static final String OFFICIAL = "1.2";
+    public static final String OFFICIAL = "1.3";
     public static final String BETA_TITLE = "Build: Beta V";
     public static final String BETA = "1.3.241024";
     public static final String SIGNATURE = " by beibeikun";
